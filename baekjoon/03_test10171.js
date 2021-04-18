3d00deafa01b68cef272c5ff1b587f8cfdd47acd
장상옥 <jso46034@naver.com>
2021-04-18 19:39:29 +0900

    Algorithm Test Commit

---
+++ baekjoon/03_test10171.js
@@ -0,0 +1,11 @@
+// console.log(`\\    /\\`);
+// console.log(` )  ( ')` );
+// console.log(`(  /  )`);
+// console.log(` \\(__)|`);
+
+console.log('\\    /\\\n )  ( \')\n(  /  )\n \\(__)|');
+
+// console.log('\\    /\\\n',
+//             ')  ( \')\n\
+// (  /  )\n',
+//             '\\(__)|\n');
