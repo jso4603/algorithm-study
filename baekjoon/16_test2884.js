b6caeb7bc589cedd5591744c9a3e669bfa8ce8e3
장상옥 <jso46034@naver.com>
2021-04-20 18:30:56 +0900

    16번 문제 수정
    16번 문제 코드 오타 수정

--- baekjoon/16_test2884.js
+++ baekjoon/16_test2884.js
@@ -30,63 +30,14 @@ rl.on('line', function (line) {
 
     // ==============================================================
 
-    // let hour = Number(input[0]);
-    // let minute = Number(input[1]);
-
-    // let date = new Date(2021, 4, 20, hour, minute);
-
-    // date.setMinutes(date.getMinutes() - 45);
-
-    // console.log(date.getHours(), date.getMinutes());
-
-    // ==============================================================
-
-    // ==============================================================
-
     let hour = Number(input[0]);
     let minute = Number(input[1]);
 
-    const date1 = new Date('December 17, 1995 03:24:01');
-    // 1995-12-16T18:24:00.000Z
+    let date = new Date(2021, 4, 20, hour, minute);
 
-    const date2 = new Date('1995-12-17T03:24:00');
-    // 1995-12-16T18:24:00.000Z
+    date.setMinutes(date.getMinutes() - 45);
 
-    console.log(date1);
-    console.log(date2);
-
-    console.log(date1 === date2);
-    // expected output: false;
-
-    console.log(date1 + date2);
-    // expected output: 0
-
-    const date3 = Date('');
-    // Tue Apr 20 2021 17:24:07 GMT+0900 (GMT+09:00)
-
-    const date4 = Date('');
-    // Tue Apr 20 2021 17:24:07 GMT+0900 (GMT+09:00)
-
-    console.log(date3);
-    console.log(date4);
-
-    console.log(date3 === date4);
-    // expected output: true;
-
-    console.log(date3 - date4);
-    // expected output: NaN;
-
-    const date5 = +new Date('December 17, 1995 03:24:00');
-    // 819138240000
-
-    const date6 = +new Date('1995-12-17T03:24:00');
-    // 819138240000
-
-    console.log(date5 === date6);
-    // expected output: true;
-
-    console.log(date5 - date6);
-    // expected output: 0
+    console.log(date.getHours(), date.getMinutes());
 
     // ==============================================================
 
